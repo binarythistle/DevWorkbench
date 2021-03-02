e6ddd366ac92128667c2aa6a549dd4d96571099d
binarythistle <les.jackson@binarythistle.com>
2021-03-02 20:18:59 +1100

    fix: catch exception and provide base64 decode message

--- Pages/Base64Convertor.cs
+++ Pages/Base64Convertor.cs
@@ -5,7 +5,7 @@ namespace DevWorkBench.Pages
 {
     public partial class Base64Convertor
     {
-        public string NonBase64Body{ get; set; }
+        public string NonBase64Body { get; set; }
         public string Base64Body { get; set; }
 
         private void ConvertToBase64()
@@ -14,15 +14,23 @@ namespace DevWorkBench.Pages
 
             var plainTextBytes = Encoding.UTF8.GetBytes(NonBase64Body);
             Base64Body = Convert.ToBase64String(plainTextBytes);
-            
+
         }
 
         private void ConvertFromBase64()
         {
             NonBase64Body = "Attempt to decode";
 
-            var base64EncodedBytes = Convert.FromBase64String(Base64Body);
-            NonBase64Body = Encoding.UTF8.GetString(base64EncodedBytes);
+            try
+            {
+                var base64EncodedBytes = Convert.FromBase64String(Base64Body);
+                NonBase64Body = Encoding.UTF8.GetString(base64EncodedBytes);
+            }
+            catch(Exception)
+            {
+                NonBase64Body = "Whoops! Probably not a Base64 string...";
+            }
+
         }
     }
 }
